0a85dcb66f334f35551cf26cf3972bf34c77d7e6
amardeepk <amardeep.k@surya-soft.comgit config --global user.email amardeep.k@surya-soft.com>
2023-09-11 17:26:50 +0530

    cargo check fmt and clippy applied

--- ASSIGNMENT1/src/main.rs
+++ ASSIGNMENT1/src/main.rs
@@ -5,30 +5,31 @@ use std::io::Read;
 #[derive(Parser)]
 pub struct Pth {
     #[clap(short)]
-    pub e:String,
+    pub e: String,
     #[clap(short)]
-    pub d:String,
+    pub d: String,
     #[clap(short)]
-    s:String,
+    s: String,
     #[clap(short)]
-    l:String,
+    l: String,
     #[clap(short)]
-    o:String,
+    o: String,
 }
 
-mod emp;
 mod dept;
-mod sal;
+mod emp;
 mod leave;
+mod sal;
 fn main() {
-    let pth:Pth=Pth::parse();
-    let mut deptmp:HashMap<i32,String>=HashMap::new();
-    dept::new(&pth,&mut deptmp).expect("Initialisation of dept db failed");
-    let mut salmp:HashMap<i32,String>=HashMap::new();
-    sal::new(&pth,&mut salmp).expect("Initialisation of sal db failed");
-    let mut lvmp:HashMap<i32,i32>=HashMap::new();
-    leave::new(&pth,&mut lvmp).expect("Initialisation of leave db failed");
-    let outst = emp::OutSt::new(&pth,&deptmp,&salmp,&lvmp).expect("Initialisation of emp db failed");
+    let pth: Pth = Pth::parse();
+    let mut deptmp: HashMap<i32, String> = HashMap::new();
+    dept::new(&pth, &mut deptmp).expect("Initialisation of dept db failed");
+    let mut salmp: HashMap<i32, String> = HashMap::new();
+    sal::new(&pth, &mut salmp).expect("Initialisation of sal db failed");
+    let mut lvmp: HashMap<i32, i32> = HashMap::new();
+    leave::new(&pth, &mut lvmp).expect("Initialisation of leave db failed");
+    let outst =
+        emp::OutSt::new(&pth, &deptmp, &salmp, &lvmp).expect("Initialisation of emp db failed");
     match outst.save(&pth) {
         Ok(_) => println!("Completed"),
         Err(why) => println!("An error occurred: {}", why),
